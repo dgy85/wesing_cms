2f47ee7519ca5e3777297ab4ffda24830fbf27d3
dgywo@qq.com <dgywo@qq.com>
2017-04-11 09:03:06 +0800

    微信接口，页面动态效果完成

---
+++ shared/libraries/wesing.php
@@ -0,0 +1,6 @@
+<?php
+class wesing
+{
+    private $key="wx421c2fcf3645f366";
+    private $secret="fdbb815ecc80f28fceb9c66bc8b8baf7";
+}
--- views/wesing/pagelist.php
+++ views/wesing/pagelist.php
@@ -10,11 +10,11 @@
                     ?>
                     <li>
                         <p class="title">
-                            <img alt="<?php echo $_cateItem['cate_name'] ?>" src="<?php echo $_cateItem['cate_thub'] ?>" width="100%" class="dark_p">
+                            <img alt="<?php echo $_cateItem['cate_name'] ?>" src="<?php echo $_cateItem['cate_thub'] ?>" width="100%" class="dark_p"><span>+</span>
                         </p>
                         <ul class='list-se'>
                             <p class="title">
-                                <img alt="<?php echo $_cateItem['cate_name'] ?>" src="<?php echo $_cateItem['cate_thub'] ?>" width="100%">
+                                <img alt="<?php echo $_cateItem['cate_name'] ?>" src="<?php echo $_cateItem['cate_thub'] ?>" width="100%"><span>-</span>
                             </p>
                             <?php if ($_cateItem['singlepage'] && $_cateItem['page']): ?>
                                 <li>
@@ -41,75 +41,19 @@
                         </ul>
                     </li>
                 <?php endforeach; ?>
-            <!--            <li>-->
-            <!--                <p class="title"><img src="pic/list_xyhd/b2.jpg" width="100%" class="dark_p"></p>-->
-            <!--                <ul class='list-se'>-->
-            <!--                    <p class="title"><img src="pic/list_xyhd/b2.jpg" width="100%"></p>-->
-            <!--                    <li>-->
-            <!--                        <img src="img/arr2.png" class="t2" />-->
-            <!--                        <p style="color:#a0a0a0;font-size:1em;text-indent:3em;">文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容</p>-->
-            <!--                        <p style="text-align:right"><a href="#" style="color:#000;font-size:1em;">详情&gt;&gt;</a></p>-->
-            <!--                        <a href="#" class="join"><img src="img/wycj.png" width="100%" /></a>-->
-            <!--                    </li>-->
-            <!--                </ul>-->
-            <!--            </li>-->
-            <!--            <li>-->
-            <!--                <p class="title"><img src="pic/list_xyhd/b3.jpg" width="100%" class="dark_p"></p>-->
-            <!--                <ul class='list-se'>-->
-            <!--                    <p class="title"><img src="pic/list_xyhd/b3.jpg" width="100%"></p>-->
-            <!--                    <li>-->
-            <!--                        <img src="img/arr2.png" class="t2" />-->
-            <!--                        <p style="color:#a0a0a0;font-size:1em;text-indent:3em;">文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容</p>-->
-            <!--                        <p style="text-align:right"><a href="#" style="color:#000;font-size:1em;">详情&gt;&gt;</a></p>-->
-            <!--                        <a href="#" class="join"><img src="img/wycj.png" width="100%" /></a>-->
-            <!--                    </li>-->
-            <!--                </ul>-->
-            <!--            </li>-->
-            <!--            <li>-->
-            <!--                <p class="title"><img src="pic/list_xyhd/b4.jpg" width="100%" class="dark_p"></p>-->
-            <!--                <ul class='list-se'>-->
-            <!--                    <p class="title"><img src="pic/list_xyhd/b4.jpg" width="100%"></p>-->
-            <!--                    <li>-->
-            <!--                        <img src="img/arr2.png" class="t2" />-->
-            <!--                        <p style="color:#a0a0a0;font-size:1em;text-indent:3em;">文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容</p>-->
-            <!--                        <p style="text-align:right"><a href="#" style="color:#000;font-size:1em;">详情&gt;&gt;</a></p>-->
-            <!--                        <a href="#" class="join"><img src="img/wycj.png" width="100%" /></a>-->
-            <!--                    </li>-->
-            <!--                </ul>-->
-            <!--            </li>-->
-            <!--            <li>-->
-            <!--                <p class="title"><img src="pic/list_xyhd/b5.jpg" width="100%" class="dark_p"></p>-->
-            <!--                <ul class='list-se'>-->
-            <!--                    <p class="title"><img src="pic/list_xyhd/b5.jpg" width="100%"></p>-->
-            <!--                    <li>-->
-            <!--                        <img src="img/arr2.png" class="t2" />-->
-            <!--                        <p style="color:#a0a0a0;font-size:1em;text-indent:3em;">文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容文字内容</p>-->
-            <!--                        <p style="text-align:right"><a href="#" style="color:#000;font-size:1em;">详情&gt;&gt;</a></p>-->
-            <!--                        <a href="#" class="join"><img src="img/wycj.png" width="100%" /></a>-->
-            <!--                    </li>-->
-            <!--                </ul>-->
-            <!--            </li>-->
-            <!--            <li>-->
-            <!--                <p class="title"><img src="pic/list_xyhd/b6.jpg" width="100%" class="dark_p"></p>-->
-            <!--                <ul class='list-se'>-->
-            <!--                    <p class="title"><img src="pic/list_xyhd/b6.jpg" width="100%"></p>-->
-            <!--                    <li>-->
-            <!--                        <ul class="list3">-->
-            <!--                            <li><a href="xq_xyzfhd.html"><span>2017-01-01</span>活动名称00001</a></li>-->
-            <!--                            <li><a href="xq_xyzfhd.html"><span>2017-01-01</span>活动名称00002</a></li>-->
-            <!--                            <li><a href="xq_xyzfhd.html"><span>2017-01-01</span>活动名称00003</a></li>-->
-            <!--                            <li><a href="xq_xyzfhd.html"><span>2017-01-01</span>活动名称00004</a></li>-->
-            <!--                            <li><a href="xq_xyzfhd.html"><span>2017-01-01</span>活动名称00005</a></li>-->
-            <!--                        </ul>-->
-            <!--                    </li>-->
-            <!--                </ul>-->
-            <!--            </li>-->
-
         </ul>
     </div>
 
 </div>
 <script src="/assets/lib/js/jquery.min.js"></script>
 <script src="/assets/apps/style.js"></script>
+<script>
+    jQuery(document).ready(function($)
+    {
+        $('.list ul li p').clickdown();
+        $('.list-se p[class=title]').clickup();
+        $('.list>ul>li').ad();
+    });
+</script>
 </body>
 </html>
