801c3a1b64a13ef27d6f0ee4d1f9d3532c1a3eed
dgywo@qq.com <dgywo@qq.com>
2017-04-20 11:06:38 +0800

    文章统计错误

--- admin/models/articles_model.php
+++ admin/models/articles_model.php
@@ -25,7 +25,7 @@ class Articles_model extends Admin_Model
     public function getList($page=1,$pagesize=10)
     {
         $whereStr = $this->whereCondition ? ' and '.implode(' and ',$this->whereCondition) : '';
-        $totalArr = $this->db->query(sprintf("select count(1) as total from %s where disabled=0 %s",$this->tableName,$whereStr))->result_array();
+        $totalArr = $this->db->query(sprintf("select count(art_id) as total from %s a,wesing_category b where a.cate_id = b.cate_id and a.disabled=0 and b.disabled=0 and b.singlepage=0 %s",$this->tableName,$whereStr))->result_array();
         $total = current($totalArr);
         $adminList = $this->db->query(sprintf("select a.*,b.cate_name,c.uname from %s a,wesing_category b,wesing_admin c where a.cate_id = b.cate_id and a.authorid=c.id and a.disabled=0 and b.disabled=0 and b.singlepage=0 %s ORDER  by art_id desc limit %d,%d" ,$this->tableName,$whereStr,($page-1)*$pagesize,$pagesize ))->result_array();
         return array(
