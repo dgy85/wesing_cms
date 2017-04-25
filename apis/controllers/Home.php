4ba2c796fbd1adb66ea30d8fcb0558c67ca39619
dgywo@qq.com <dgywo@qq.com>
2017-04-25 18:43:08 +0800

    头信息读取

--- apis/controllers/Home.php
+++ apis/controllers/Home.php
@@ -1,12 +1,37 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Home extends CI_Controller {
+class Home extends API_Controller {
 
     public function index()
 	{
-        logmsg("");
-        log_message('error','测试的error信息');
-        var_dump($this->config->item("log_threshold"));
+	    $this->_responseType='xml';
+	    $this->_response(array(
+	        "responseCode"=>'40096230',
+            'responseMsg'=>'发生了未知的错误',
+            'erroruri'=>$_SERVER['HTTP_USER_AGENT'],
+            'items'=>array(
+                array(
+                    'ID'=>1,
+                    'Name'=>'BBQ',
+                    'Amt'=>2342
+                ),
+                array(
+                    'ID'=>12,
+                    'Name'=>'BBQ',
+                    'Amt'=>242
+                ),
+                array(
+                    'ID'=>13,
+                    'Name'=>'BB1Q',
+                    'Amt'=>232
+                ),
+                array(
+                    'ID'=>14,
+                    'Name'=>'BB2Q',
+                    'Amt'=>232
+                ),
+            )
+            ));
 	}
 }
